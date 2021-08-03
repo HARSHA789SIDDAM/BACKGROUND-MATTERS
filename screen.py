13a5ef37d2a9b8a878dc4d9f522363f1cbecf6af
HARSHA789SIDDAM <72926700+HARSHA789SIDDAM@users.noreply.github.com>
2021-08-03 17:49:11 +0530

    Update screen.py

--- screen.py
+++ screen.py
@@ -2,13 +2,8 @@ import cv2
 import time
 import numpy as np
 
-fourcc = cv2.VideoWriter_fourcc (*'XVID')
-output_file = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
-
 cap = cv2.VideoCapture(0)
-
-time.sleep(2)
-bg = 0
+img = cv2.imread("")
 
 for i in range(60):
 
